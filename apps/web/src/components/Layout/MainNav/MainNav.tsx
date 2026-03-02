4e7cc9925b5c9706293d058dfd816e0a0f03c4e3
Lee Hitchcock <lee.hitchcock2041@gmail.com>
2026-03-02 12:46:13 +0000

    feat(web): use Button component from @mythmaker/ui in MainNav

--- apps/web/src/components/Layout/MainNav/MainNav.tsx
+++ apps/web/src/components/Layout/MainNav/MainNav.tsx
@@ -1,14 +1,15 @@
 import { Link } from '@tanstack/react-router'
+import { Button } from '@mythmaker/ui'
 
 export const MainNav = () => {
 	return (
 		<>
 			<div className='p-2 flex flex-row justify-end gap-2'>
 				<Link to='/' className='[&.active]:font-bold'>
-					Home
+					<Button variant='primary'>Home</Button>
 				</Link>{' '}
 				<Link to='/about' className='[&.active]:font-bold'>
-					About
+					<Button variant='primary'>About</Button>
 				</Link>
 			</div>
 		</>
