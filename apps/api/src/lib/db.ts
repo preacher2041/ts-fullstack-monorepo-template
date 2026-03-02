6eb92caa45dfe76694d92bb6f431c68a9c56cdab
Lee Hitchcock <lee.hitchcock2041@gmail.com>
2026-03-02 14:56:57 +0000

    fix(prisma): Update prisma connection string

--- apps/api/src/lib/db.ts
+++ apps/api/src/lib/db.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 import 'dotenv/config';
@@ -10,8 +9,7 @@ if (!connectionString) {
 	throw new Error('DATABASE_URL environment variable is not set');
 }
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
