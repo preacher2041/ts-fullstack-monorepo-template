de0704d1df778b351b5d6a768ee0561b09297c1f
Lee Hitchcock <lee.hitchcock2041@gmail.com>
2026-04-17 14:29:14 +0100

    fix: correct validation messages in registration form schema

--- apps/web/src/features/Auth/components/Registration/RegistrationContainer.tsx
+++ apps/web/src/features/Auth/components/Registration/RegistrationContainer.tsx
@@ -4,13 +4,13 @@ import { RegistrationForm } from './RegistrationForm'
 
 const registrationFormSchema = z.object({
 	firstName: z.string().min(1, 'First name is required'),
-	lastName: z.string().min(1, 'First name is required'),
+	lastName: z.string().min(1, 'Last name is required'),
 	email: z.email({
 		pattern: z.regexes.html5Email,
 		error: 'Email address must be a valid email',
 	}),
 	dob: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, 'Invalid date format'),
-	username: z.string().min(1, 'First name is required'),
+	username: z.string().min(1, 'Username name is required'),
 	password: z.string().min(8, 'Password must be at least 8 characters long'),
 })
 
