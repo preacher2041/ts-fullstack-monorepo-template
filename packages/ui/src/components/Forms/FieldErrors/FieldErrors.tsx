0344e1080894f7e281a7f2d8a0ce1363fed71c8e
Lee Hitchcock <lee.hitchcock2041@gmail.com>
2026-04-25 13:34:10 +0100

    P2041-17 - fix: improve error handling in FieldErrors component

--- packages/ui/src/components/Forms/FieldErrors/FieldErrors.tsx
+++ packages/ui/src/components/Forms/FieldErrors/FieldErrors.tsx
@@ -9,13 +9,15 @@ type FieldErrorProps = {
 export const FieldErrors = ({ meta, name, hideErrors }: FieldErrorProps) => {
 	if (hideErrors) return null
 
-	const uniqueErrors = [...new Set(meta.errors.map((err) => err.message))]
+	const uniqueErrors = [
+		...new Set(meta.errors.filter(Boolean).map((err) => String(err))),
+	]
 	if (uniqueErrors.length === 0) return null
 
 	return (
 		<div id={`${name}-error`}>
 			{uniqueErrors.map((message, index) => (
-				<p key={index} className='mt-2 text-destructive'>
+				<p key={index} className='text-destructive'>
 					{message}
 				</p>
 			))}
