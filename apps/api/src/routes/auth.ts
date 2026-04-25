0ee05526cb8529f2cd0b0924f9827d8c5fb59d23
Lee Hitchcock <lee.hitchcock2041@gmail.com>
2026-04-25 21:57:02 +0100

    P2041-19 - fix: remove authentication middleware from session route

--- apps/api/src/routes/auth.ts
+++ apps/api/src/routes/auth.ts
@@ -6,13 +6,12 @@ import {
 	loginUserController,
 	logoutUserController,
 } from '../controllers/auth.controllers'
-import { addAuthMiddleware } from '../middleware/auth'
 import { validate } from '../middleware/validate'
 
 const router: express.Router = express.Router()
 
 router.post('/login', validate(loginSchema), loginUserController)
 router.post('/logout', logoutUserController)
-router.get('/session', addAuthMiddleware, getCurrentSessionController)
+router.get('/session', getCurrentSessionController)
 
 export default router
